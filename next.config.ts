9977edb7737660b5c88e30e885d3db8ef3160f8b
Yves-Developer <yvesmugisha09@gmail.com>
2026-03-29 15:15:18 +0200

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // @ts-expect-error: Built-in next param not typed
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
